68cfd2ba745ee57d336a81d7f8f4d110d552320f
Tommy Madden <madde120@mail.chapman.edu>
2024-09-01 11:47:42 -0600

    more changes to darts

--- src/views/darts/DartWallPage.jsx
+++ src/views/darts/DartWallPage.jsx
@@ -9,6 +9,8 @@ import { Header } from '../../components/Header';
 import './darts.css';
 import { getDarts, addDart, updateDart } from './server';
 import { DartModal } from './DartModal';
+import { Button } from '@mui/material';
+import classNames from 'classnames';
 
 export const DartWallPage = () => {
 
@@ -17,10 +19,28 @@ export const DartWallPage = () => {
     const [modalOpen, setModalOpen] = useState(false);
     const [board, setBoard] = useState('Shows');
     const [dartItems, setDartItems] = useState([]);
+    const [shuffleDart, setShuffleDart] = useState([]);
     const [activeDart, setActiveDart] = useState(initialFields);
 
     const fetchDarts = async(board) => {
-        setDartItems(await getDarts(board));
+        let newDartItems = await getDarts(board);
+
+        newDartItems = newDartItems.sort((a, b) => {
+            if (a.rating > b.rating) {
+                return -1;
+            } else if (b.rating > a.rating) {
+                return 1;
+            }
+            return 0;
+        });
+    
+        newDartItems.forEach((item) => {
+            if (item.metrics[sortMetric] === 0) {
+                item.metrics[sortMetric] = Math.floor(Math.random() * 8) + 1;
+            }
+        });
+
+        setDartItems(newDartItems);
     }
 
     useEffect(() => {
@@ -31,31 +51,15 @@ export const DartWallPage = () => {
             setModalOpen(false);
             setSortMetric('heartlighted');
         }
-    }, []);
-
-    useEffect(() => { fetchDarts(board); }, [board]);
+    }, [board]);
 
     useEffect(() => {
         setModalOpen(!!activeDart?._id);
     }, [activeDart])
 
-    let darts = dartItems.sort((a, b) => {
-        if (a.rating > b.rating) {
-            return -1;
-        } else if (b.rating > a.rating) {
-            return 1;
-        }
-        return 0;
-    });
-
-    darts.forEach((item) => {
-        if (item.metrics?.heartlighted === 0) {
-            item.metrics.heartlighted = Math.floor(Math.random() * 8) + 1;
-        }
-    })
-
-    const DartItem = ({ name, enneagram, onClick }) =>
-        <div className={`darts__item darts__item--${enneagram.toString(10)}`} onClick={onClick}>
+    const DartItem = ({ name, enneagram, onClick, shuffleDart }) =>
+        <div className={classNames(`darts__item darts__item--${enneagram.toString(10)}`,
+            { 'darts__item--shuffled': shuffleDart })} onClick={onClick}>
             {name}
         </div>
 
@@ -97,40 +101,115 @@ export const DartWallPage = () => {
                         </Select>
                     </FormControl>
                 </div>
+                <Button onClick={() => {
+                    const newShuffleDart = dartItems[Math.floor(Math.random() * dartItems.length)];
+                    const sortedDarts = dartItems.sort((a, b) => {
+                        if (a.metrics[sortMetric] > b.metrics[sortMetric]) {
+                            return -1;
+                        } else if (b.metrics[sortMetric] > a.metrics[sortMetric]) {
+                            return 1;
+                        }
+                        return 0;
+                    });
+                    
+                    console.log(sortedDarts);
+                    let i = 0;
+                    const goToNextDart = () => {
+                        setShuffleDart(sortedDarts[i]);
+                        if (i < sortedDarts.length) {
+                            i++;
+                        } else {
+                            i = 0;
+                        }
+                    };
+
+                    const interval = setInterval(goToNextDart, 100);
+                    const timeoutLength = Math.floor((Math.random() * 3000)) + 1000;
+
+                    setTimeout(() => {
+                        clearInterval(interval);
+                        const newInterval = setInterval(goToNextDart, 500);
+                        
+                        setTimeout(() => {
+                            clearInterval(newInterval);
+                            setShuffleDart(newShuffleDart);
+                        }, 2500);
+                    }, timeoutLength);
+                }}>Shuffle!</Button>
                 <button className='darts__btn' onClick={() => setModalOpen(true)}>+</button>
             </div>
             <div className="darts">
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 8)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 8).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 7)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 7).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 6)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 6).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 5)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 5).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 4)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 4).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 3)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 3).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 2)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 2).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
                 <div className="darts__row">
-                    {darts.filter(({ metrics }) => metrics[sortMetric] === 1)
-                        .map((props) => <DartItem {...props} key={props['_id']} onClick={() => setActiveDart(props)} />)}
+                    {dartItems.filter(({ metrics }) => metrics[sortMetric] === 1).map((props) =>
+                        <DartItem
+                            {...props}
+                            key={props['_id']}
+                            onClick={() => setActiveDart(props)}
+                            shuffleDart={props['_id'] === shuffleDart?._id}
+                        />)}
                 </div>
             </div>
         </div>
