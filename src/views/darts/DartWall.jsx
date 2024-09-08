d1d4b765e5fd302dc0b63672e52f472630ce870b
Tommy Madden <madde120@mail.chapman.edu>
2024-09-08 09:40:47 -0600

    swiping v1

--- src/views/darts/DartWall.jsx
+++ src/views/darts/DartWall.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
 const DartItem = ({ name, enneagram, onClick, shuffleDart }) =>
@@ -10,10 +10,12 @@ const DartItem = ({ name, enneagram, onClick, shuffleDart }) =>
 export const DartWall = ({ dartItems, setActiveDart, shuffleDart, sortMetric }) => {
     // An array of rows -- each will be populated based on sortMetric (8 -> 0)
     let dartWall = [[], [], [], [], [], [], [], [], []];
+    let longestRow = 0;
 
     dartItems?.forEach((item) => {
         const row = 7 - item?.metrics[sortMetric];
         const rowLength = dartWall[row].length;
+        if (rowLength > longestRow) { longestRow = rowLength; }
         
         // In case you ever want to do it in a more position: absolute way!
         //
@@ -32,13 +34,49 @@ export const DartWall = ({ dartItems, setActiveDart, shuffleDart, sortMetric }) 
 
     console.log(dartWall);
 
+    // mobile state..have to thinkabout this
+    const [touchStart, setTouchStart] = useState(null);
+    const [touchEnd, setTouchEnd] = useState(null);
+
+    const [scrollX, setScrollX] = useState(0);
+
+    // the required distance between touchStart and touchEnd to be detected as a swipe
+    const minSwipeDistance = 50 
+    
+    const onTouchStart = (e) => {
+        setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+        setTouchStart(e.targetTouches[0].clientX)
+      }
+      
+      const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+      
+      const onTouchEnd = () => {
+        if (!touchStart || !touchEnd) return;
+        const distance = touchStart - touchEnd;
+        const isLeftSwipe = distance > minSwipeDistance;
+        const isRightSwipe = distance < -minSwipeDistance;
+        if (isLeftSwipe || isRightSwipe) console.log('swipe', isLeftSwipe ? 'left' : 'right')
+
+        if (isLeftSwipe && longestRow >= ((scrollX + 1) * 5)) {
+            setScrollX(scrollX + 1);
+        } else if (isRightSwipe && scrollX > 0) {
+            setScrollX(scrollX - 1);
+        }
+      }
+
     return (
-        <div className="darts">
-            {dartWall.map((row) => {
+        <div className="darts" onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
+            {dartWall.map((row, rowIndex) => {
                 if (row.length === 0) return null;
+                
+                if (document.body.scrollWidth <= 768) {
+                    row = row.slice((scrollX * 5), (scrollX * 5) + 5) || [];
+                } else {
+                    row = row.slice((scrollX * 10), (scrollX * 10) + 10) || [];
+                }
 
                 return (
-                    <div className="darts__row">
+                    <div className="darts__row" key={`row-${rowIndex}`}>
                         {row.map((item) =>
                             <DartItem
                                 {...item}
